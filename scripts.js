323d593bffefffa3ba4553fd1e5d502f7ac570cd
caglenoah <caglenoah@gmail.com>
2023-06-23 00:20:21 -0600

    progress

--- scripts.js
+++ scripts.js
@@ -4,3 +4,17 @@ new URLSearchParams(window.location.search).forEach((value, name) => {
     placeForResults.append(`${name} : ${value}`)
     placeForResults.append(document.createElement("br"))
 })
+// Define the checkBirthday function using arrow function syntax
+const checkBirthday = () => {
+  // Get today's date and format it as "YYYY-MM-DD"
+    const today = new Date().toISOString().slice(0, 10);
+  // Get the birthdate entered by the user
+    const birthdate = document.getElementById("birthdate").value;
+      // Compare the birthdate with today's date and display a message
+    if (birthdate === today) {
+        document.getElementById("message").textContent = "Happy birthday!";
+        else {
+        document.getElementById("message").textContent = "have a great day!";
+        }
+}
+}
